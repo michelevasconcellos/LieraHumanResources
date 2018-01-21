f800e00e71727ad3ca7a70746dab9304c57b5ff5
Michele Ribeiro Queiroz Vasconcellos <TAE11517@somoseducacao.com.br>
2018-01-21 09:31:49 -0200

    Criação de COnsumo de Serviços

---
+++ LieraHumanResources/index3_controller.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    'use strict';
+    angular
+        .module('lierahuanresources')
+        .controller('index3ctrl', index3ctrl);
+
+    index3ctrl.$inject = ['index3_factory', 'item', 'id', '$uibModalInstance', 'ErroFactory', '$stateParams'];
+
+    
+})();
---
+++ LieraHumanResources/index3_factory.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+    angular.module('lierahuanresources').factory('index3_factory', index3_factory);
+
+    EstudoCancelamentoFactory.$inject = ['$http', 'SETTINGS'];
+    var routePrefix = 'rentabilidade/incluir/';
+    function EstudoCancelamentoFactory($http, SETTINGS) {
+        return {
+            realizarCancelamento: _realizarCancelamento,
+           
+        };
+
+        function _realizarCancelamento(id, mensagem) {
+            var url = String.format('{0}rentabilidade/cancelarproposta?id={1}&justificativa={2}', SETTINGS.SERVICE_URL, id, mensagem);
+            return $http.get(url);
+        }
+
+    }
+})();
